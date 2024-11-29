fbd11d8821bfbe5b29279d1b72d0890ac0b104a9
Elba Fernandez <elba.fdez.lpz@gmail.com>
2024-11-29 16:17:38 +0100

    Archivo en proceso del dragon dentro de Recursos

--- BasicProject/Assets/Scripts/MObstaculos.cs
+++ BasicProject/Assets/Scripts/MObstaculos.cs
@@ -5,20 +5,19 @@ using UnityEngine;
 public class MObstaculos : MonoBehaviour
 {
     private float VMovimiento = 8f;
-    public GameObject spawnerObstaculos;
     void Start()
     {
-        
+
     }
 
-    
+
     void Update()
     {
         transform.Translate(Vector3.left * VMovimiento * Time.deltaTime);
     }
 
-    /*private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject);
-    }*/
+        Destroy(collision.gameObject);
+    }
 }
