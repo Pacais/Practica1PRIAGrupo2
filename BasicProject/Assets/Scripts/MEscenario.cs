f6d4368e31f8e21d416ce8fbbcdcf6457007a7b4
Elba Fernandez <elba.fdez.lpz@gmail.com>
2024-12-18 15:31:54 +0100

    Cambio en los sprites de fodo en el proyecto

--- BasicProject/Assets/Scripts/MEscenario.cs
+++ BasicProject/Assets/Scripts/MEscenario.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class MEscenario : MonoBehaviour
 {
     private float speed;
-    
+
     void Update()
     {
-        speed = GameManager.gameManager.VMovimiento; 
+        speed = GameManager.gameManager.VMovimiento;
         transform.Translate(Vector3.left * speed * Time.deltaTime);
 
         // Repeticion de suelo
-        if(transform.position.x <= -28f)
+        if (transform.position.x <= -22.3f)
         {
-            transform.Translate(Vector3.right * 28f * 2f);
+            transform.Translate(Vector3.right * 22.3f * 2f);
         }
     }
 }
